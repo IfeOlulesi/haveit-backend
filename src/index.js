722b2cba43c80281e200cafbcf38ddf09f059736
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-07-08 19:02:38 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import category from './routes/category.js';
 
 const Haveit = express();
 const PORT = process.env.PORT || 3100;
@@ -9,6 +8,6 @@ Haveit.get('/api/v1/', (_,res) => {
     res.send("Hello Welcome to Haveit")
 })
 
-Haveit.use('/api/v1/category', category)
+
 
 Haveit.listen(PORT, () => console.log(`Haveit is listing at Port: ${PORT}`))
