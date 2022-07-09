d57fd88ae9e60d0f5eb18b00c72d4e8a88c256fe
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-07-09 20:53:34 +0100

    setup product route

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import swaggerSpecs from './config/swagger.js'
 
 // Routes
 import category from './api/routes/category.js';
+import product from './api/routes/product.js'
 
 
 const Haveit = express();
@@ -30,5 +31,6 @@ Haveit.get('/api/v1', (_, res) => { res.status(302).redirect('/api/v1/doc') })
 
 Haveit.use('/api/v1/doc', SwaggerUi.serve, SwaggerUi.setup(swaggerSpecs))
 Haveit.use('/api/v1/category', category)
+Haveit.use('/api/v1/product', product)
 
 Start()
