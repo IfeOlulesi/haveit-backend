e8fd0a2374ded811cfcdb6387bf28dea5eac1413
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-07-09 19:34:50 +0100

    modified swagger config

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,11 +11,41 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3100"
+                url: "http://localhost:3100/api/v1",
+                description: "Local Server"
+            },
+            {
+                url: "https://haveit-backend.herokuapp.com/api/v1",
+                description: "Production Server"
             }
         ],
+
+        tags: [
+            {
+                name: "Product",
+                description: "Product end points"
+            },
+            {
+                name: "Category",
+                description: "Category end point"
+            },
+            {
+                name: "Customer",
+                description: "Customer end point"
+            },
+            {
+                name: "Cart",
+                description: "Cart end point"
+            },
+            {
+                name: "Admin",
+                description: "Admin end point"
+            }
+        ]
     },
-    apis: ["../docs/*.js"]
+
+    apis: ["src/docs/*.js"]
+    
 }
 
 export default swaggerJSDoc(options)
