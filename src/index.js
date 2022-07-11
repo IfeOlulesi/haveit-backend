1c23001dd0c202cdb46d7098300d5c558d4a4ddd
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-07-11 22:51:10 +0100

    added morgan and connected to prod DB

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { connectDB } from './config/dbconnection.js';
+import errorHandler from './api/middleware/errorHandler.js';
+import morgan from 'morgan';
 
 // Swagger
 import SwaggerUi from 'swagger-ui-express';
-import swaggerSpecs from './config/swagger.js'
+import swaggerSpecs from './config/swagger.js';
 
 // Routes
 import category from './api/routes/category.js';
-import product from './api/routes/product.js'
+import product from './api/routes/product.js';
 
 
 const Haveit = express();
@@ -15,7 +17,7 @@ const PORT = process.env.PORT || 3100;
 
 const Start = async () => {
     try {
-        await connectDB().then(() => console.log("connected to DB"))
+        await connectDB()
         Haveit.listen(PORT, () => console.log(`Haveit is listing at Port: ${PORT}`))
     } catch (error) {
         console.log(error)        
@@ -24,6 +26,7 @@ const Start = async () => {
 
 Haveit.use(express.json());
 Haveit.use(express.urlencoded({ extended: true }));
+Haveit.use(morgan('combined'))
 
 
 Haveit.get('/', (_, res) => { res.status(302).redirect('/api/v1/doc') })
@@ -32,5 +35,8 @@ Haveit.get('/api/v1', (_, res) => { res.status(302).redirect('/api/v1/doc') })
 Haveit.use('/api/v1/doc', SwaggerUi.serve, SwaggerUi.setup(swaggerSpecs))
 Haveit.use('/api/v1/category', category)
 Haveit.use('/api/v1/product', product)
+
+// Error Handler
+Haveit.use(errorHandler)
 
 Start()
